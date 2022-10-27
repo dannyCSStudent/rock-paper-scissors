8c744ffa3091f2bb7bee50d84b9c72a6da293a51
Dan Villegas <danv8605@gmail.com>
2022-10-27 15:15:28 -0500

    Added JavaScript and style
    Got the game working okay. Added some background
    images that I downloaded from pixibay.com.

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ function disableButtons() {
     document.getElementById("rock").disabled = true;
     document.getElementById("paper").disabled = true;
     document.getElementById("scissors").disabled = true;
+    document.getElementById("computer-pick").disabled = true;
+    document.getElementById("play").textContent = "Play";
+    document.getElementById("play").style.border = "1px solid aquamarine";
+    document.getElementById("rock").style.border = "1px solid aquamarine";
+    document.getElementById("paper").style.border = "1px solid aquamarine";
+    document.getElementById("scissors").style.border = "1px solid aquamarine";
+    document.getElementById("computer-pick").style.border = "1px solid aquamarine";
 };
 
 const buttons = document.querySelectorAll("button");
@@ -19,15 +26,21 @@ buttons.forEach((button) => {
                 reset();
                 break;
             case "rock":
+                document.getElementById("rock").style.border = "1px solid aquamarine";
+                document.getElementById("paper").style.border = "0";
+                document.getElementById("scissors").style.border = "0";
                 updateScore(button.id);
                 break;
             case "paper":
+                document.getElementById("rock").style.border = "0";
+                document.getElementById("paper").style.border = "1px solid aquamarine";
+                document.getElementById("scissors").style.border = "0";
                 updateScore(button.id)
                 break;
             case "scissors":
-                updateScore(button.id)
-                break;
-            case "computer":
+                document.getElementById("rock").style.border = "0";
+                document.getElementById("paper").style.border = "0";
+                document.getElementById("scissors").style.border = "1px solid aquamarine";
                 updateScore(button.id)
                 break;
         }         
@@ -36,83 +49,104 @@ buttons.forEach((button) => {
 
 function updateScore(player) {
     gameCount++;
-    document.getElementById("round").textContent = `Round: ${gameCount + 1}`;
+    
+    document.getElementById("game-label").textContent = `Round: ${gameCount + 1}`;
     let computer = getComputerChoice();
+    document.getElementById("computer-pick").textContent =  computer;
+    document.getElementById("computer-pick").style.border = "1px solid aquamarine";
     if (playRound(player, computer) === true) {
+        // computer won
         computerCount++;
-        document.getElementById("computer").textContent = `${computer}`;
         document.getElementById("score").textContent = `${playerCount} - ${computerCount}`;
-        document.getElementById("player-board").textContent = "Loser";
-        document.getElementById("computer-board").textContent = "Winner";
+        document.getElementById("player").textContent = "Loser";
+        document.getElementById("computer").textContent = "Winner";
     }
 
     else if (playRound(player, computer) === false) {
+        // player won
         playerCount++;
-        document.getElementById("computer").textContent = `${computer}`;
+        document.getElementById("player").textContent = "Winner";
+        document.getElementById("computer").textContent = "Loser";  
         document.getElementById("score").textContent = `${playerCount} - ${computerCount}`;
-        document.getElementById("player-board").textContent = "Winner";
-        document.getElementById("computer-board").textContent = "Loser";    
+          
     }
     else {
-        document.getElementById("computer").textContent = `${computer}`;
+        // tie        
         document.getElementById("score").textContent = `${playerCount} - ${computerCount}`;
-        document.getElementById("player-board").textContent = "Tie";
-        document.getElementById("computer-board").textContent = "Tie";
+        document.getElementById("player").textContent = "Tie";
+        document.getElementById("computer").textContent = "Tie";
     }
     if (playerCount === 5 || computerCount === 5) {
         disableButtons();
+        clearCounters();
         document.getElementById("game-label").textContent = "Game Over";
         document.getElementById("play").textContent = "Play Again";
+        document.getElementById("play").disabled = false;
         if (playerCount > computerCount) {
-            document.getElementById("round").textContent = "You Win!";
-            document.getElementById("player-board").textContent = "Winner";
-            document.getElementById("computer-board").textContent = "Loser";
-        }
-        else if (playerCount < computerCount) {
-            document.getElementById("round").textContent = "You Lose!";
-            document.getElementById("computer-board").textContent = "Winner";
-            document.getElementById("player-board").textContent = "Loser";
+            document.getElementById("player").textContent = "Winner";
+            document.getElementById("computer").textContent = "Loser";
         }
         else {
-            document.getElementById("round").textContent = "Tie";
-            document.getElementById("computer-board").textContent = "Tie";
-            document.getElementById("player-board").textContent = "tie";
+            document.getElementById("computer").textContent = "Winner";
+            document.getElementById("player").textContent = "Loser";
         }
-        document.getElementById("play").disabled = false;
+        
     }
 }
 
 function reset() {
+    pic();
+    document.getElementsByName("computer-pick").textContent = "Thinking";
     document.getElementById("score").textContent = "0 - 0";
-    document.getElementById("round").textContent = "Round: 0";
-    document.getElementById("game-label").textContent = "Play 5 Rounds";
-    document.getElementById("player-board").textContent = "You";
-    document.getElementById("computer-board").textContent = "Computer";
+    document.getElementById("computer-pick").textContent = "Thinking";
+    document.getElementById("game-label").textContent = "Win 5 Rounds";
+    document.getElementById("player").textContent = "You";
+    document.getElementById("computer").textContent = "Computer";
     clearCounters();
+
 }
 
 
 function play() {
+    document.getElementById("play").style.border = "1px solid blue";
+    document.getElementById("rock").style.border = "1px solid aquamarine";
+    document.getElementById("paper").style.border = "1px solid aquamarine";
+    document.getElementById("scissors").style.border = "1px solid aquamarine";
+    document.getElementById("computer-pick").style.border = "1px solid aquamarine";
+
     document.getElementById("play").disabled = true;
     document.getElementById("rock").disabled = false;
     document.getElementById("paper").disabled = false;
     document.getElementById("scissors").disabled = false;
-    document.getElementById("game-label").textContent = "Play 5 Round";
-    document.getElementById("round").textContent = "Round: 1";
-    document.getElementById("player-board").textContent = "You";
-    document.getElementById("computer-board").contains = "Computer";
-    document.getElementById("rock").style.border = "2px solid green";
-    document.getElementById("paper").style.border = "2px solid green";
-    document.getElementById("scissors").style.border = "2px solid green";
+    document.getElementById("game-label").textContent = "Round 1";
+    document.getElementById("play").textContent = "Play";
+    document.getElementById("player").textContent = "You"
+    document.getElementById("computer").textContent = "Computer";
 }
 
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
+    const choices = ["Rock", "Paper", "Scissors"];
     let roll = Math.floor(Math.random() * 3);
     let computerChoice = choices[roll];
     return computerChoice
 }
 
+function pic() {
+
+    const choice = [
+        "./images/peru-1460618_1920.jpg", 
+        "./images/brazil-305531_1280.png",
+        "./images/japan-1460334_1920.jpg",
+        "./images/chili-1460323_1920.jpg",
+        "./images/romania-1460575_1920.jpg"
+    ];
+    $("#container").css({
+        "background": "url("+choice[Math.floor(Math.random() *
+            choice.length)] +") no-repeat",
+            "background-position":"100%",
+            "background-size":"cover"
+    });
+}
 function clearCounters() {
     playerCount = 0;
     computerCount = 0;
@@ -121,23 +155,24 @@ function clearCounters() {
 }
 
 function playRound(PlayerSelection, ComputerSelection) {
-    if (PlayerSelection === "rock" && ComputerSelection === "paper") {
+    
+    if (PlayerSelection === "rock" && ComputerSelection === "Paper") {
         return true;
     }
-    else if (PlayerSelection === "rock" && ComputerSelection === "scissors") {
+    else if (PlayerSelection === "rock" && ComputerSelection === "Scissors") {
         return false;
     }
-    else if (PlayerSelection === "paper" && ComputerSelection === "scissors") {
+    else if (PlayerSelection === "paper" && ComputerSelection === "Scissors") {
         return true;
     }
-    else if (PlayerSelection === "paper" && ComputerSelection === "rock") {
+    else if (PlayerSelection === "paper" && ComputerSelection === "Rock") {
         
         return false;
     }
-    else if (PlayerSelection === "scissors" && ComputerSelection === "rock") {
+    else if (PlayerSelection === "scissors" && ComputerSelection === "Rock") {
         return true;
     }
-    else if (PlayerSelection === "scissors" && ComputerSelection === "paper") {
+    else if (PlayerSelection === "scissors" && ComputerSelection === "Paper") {
         
         return false;
     }
